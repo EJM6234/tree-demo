8d95e0c2d858c18876e3b34afd74578b0109d098
EJ Morgan <edwardmorgan@EJs-MacBook-Pro.local>
2018-10-21 12:10:09 -0400

    stuff

--- Client/build/service-worker.js
+++ Client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a075c6e95e4e0161d6cbb2b1c3bcddcd.js"
+  "/build/precache-manifest.a075c6e95e4e0161d6cbb2b1c3bcddcd.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/build/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
