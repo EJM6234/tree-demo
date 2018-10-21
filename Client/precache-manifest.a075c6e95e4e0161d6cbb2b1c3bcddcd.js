47f91d35eaddbc102d5eccfd7aac2a6776540725
EJ Morgan <edwardmorgan@EJs-MacBook-Pro.local>
2018-10-21 12:14:42 -0400

    Heroku Fix

---
+++ Client/precache-manifest.a075c6e95e4e0161d6cbb2b1c3bcddcd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/build/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0fc0565ceec21b9bff96",
+    "url": "/build/static/js/main.0fc0565c.chunk.js"
+  },
+  {
+    "revision": "1c7ee23a42a6a9f0982f",
+    "url": "/build/static/js/1.1c7ee23a.chunk.js"
+  },
+  {
+    "revision": "0fc0565ceec21b9bff96",
+    "url": "/build/static/css/main.6b490377.chunk.css"
+  },
+  {
+    "revision": "61a3d31fafb58fd91c0c9a7158e1355b",
+    "url": "/build/index.html"
+  }
+];
